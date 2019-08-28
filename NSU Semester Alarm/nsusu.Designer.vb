bc918b4f0013ec0cadd559376180fa87387813c4
Samiur Rahman Sami <49389786+srs404@users.noreply.github.com>
2019-08-29 00:32:17 +0600

    NSU Semester Alarm
    A simple program to notify users of their class schedules in an annoying way.

---
+++ NSU Semester Alarm/nsusu.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class nsusu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.checktimedate = New System.Windows.Forms.Timer(Me.components)
+        Me.titlenme = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.section = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'checktimedate
+        '
+        '
+        'titlenme
+        '
+        Me.titlenme.AutoSize = True
+        Me.titlenme.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.titlenme.ForeColor = System.Drawing.Color.White
+        Me.titlenme.Location = New System.Drawing.Point(215, 37)
+        Me.titlenme.Name = "titlenme"
+        Me.titlenme.Size = New System.Drawing.Size(749, 51)
+        Me.titlenme.TabIndex = 0
+        Me.titlenme.Text = "Class: CSE115 || Section 6 || Fall 18"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(339, 397)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(507, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Class is Starting in 10 Minutes!!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(227, 148)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(748, 135)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "00:00:00 PM"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderSize = 4
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(1092, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 54)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'section
+        '
+        Me.section.AutoSize = True
+        Me.section.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.section.ForeColor = System.Drawing.Color.White
+        Me.section.Location = New System.Drawing.Point(438, 318)
+        Me.section.Name = "section"
+        Me.section.Size = New System.Drawing.Size(311, 46)
+        Me.section.TabIndex = 0
+        Me.section.Text = "Room: SAC211"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1205, 456)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.section)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.titlenme)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "NSU Class Alarm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents checktimedate As Timer
+    Friend WithEvents titlenme As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents section As Label
+End Class
---
+++ NSU Semester Alarm/nsusu.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Public Class nsusu
+    Dim tody As String = DateTime.Now.ToString("dddd")
+    Dim nowtime As String = DateTime.Now.ToString("hh:mm:ss tt")
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Visible = False
+        checktimedate.Start()
+    End Sub
+
+    Private Sub checktimedate_Tick(sender As Object, e As EventArgs) Handles checktimedate.Tick
+        Dim nowtime As String = DateTime.Now.ToString("hh:mm:ss tt")
+        If tody = "Tuesday" And nowtime >= "02:30:00 PM" And nowtime <= "04:00:00 PM" Then
+            Me.Visible = True
+            Me.TopMost = True
+            Label2.Text = nowtime
+            section.Text = "Room: SAC513"
+        ElseIf tody = "Sunday" And nowtime >= "02:30:00 PM" And nowtime <= "04:00:00 PM" Then
+            Me.Visible = True
+            Me.TopMost = True
+            Label2.Text = nowtime
+            section.Text = "Room: SAC513"
+        ElseIf tody = "Monday" And nowtime >= "04:10:00 PM" And nowtime <= "05:50:00 PM" Then
+            Me.Visible = True
+            Me.TopMost = True
+            Label2.Text = nowtime
+            section.Text = "Room: SAC211"
+        ElseIf tody = "Wednesday" And nowtime >= "04:10:00 PM" And nowtime <= "05:50:00 PM" Then
+            Me.Visible = True
+            Me.TopMost = True
+            Label2.Text = nowtime
+            section.Text = "Room: SAC211"
+        Else
+            Me.Visible = True
+            'My.Computer.Audio.Play(My.Resources.alarm, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
